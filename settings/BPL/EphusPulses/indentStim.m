4f637966855bbb4694cce219b0c409013a1d4bfe
Brendan <blehnert@post.harvard.edu>
2014-11-17 18:36:40 -0500

    updated indent and pgrid routines

--- settings/BPL/EphusPulses/indentStim.m
+++ settings/BPL/EphusPulses/indentStim.m
@@ -2,11 +2,11 @@ function [Lstep, fsteps] = indentStim()
 
 fConv = 50; %mN/V
 eConv = 1000 ; %V/V
-forces = [2,8,32,128]./ fConv;
+forces = [4,12,36,108]./ fConv;
 forces = forces .* eConv;
 lVoltage =9500;
-fsteps = ephusStimuli.makeStepSeries(forces,.08, .07,40000);
-Lstep = ephusStimuli.lpFilter(ephusStimuli.makeStep(.04, .85, .05, lVoltage,40000), 50);
+fsteps = ephusStimuli.makeStepSeries(forces,.08, .08,40000);
+Lstep = ephusStimuli.lpFilter(ephusStimuli.makeStep(.03, .85, .05, lVoltage,40000), 50);
 
 
 end
